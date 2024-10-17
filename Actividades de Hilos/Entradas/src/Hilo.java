bf880a3c9a29e5003f147e9829efca610f2b7556
davpicroc <davpicroc@alu.edu.gva.es>
2024-10-17 12:08:45 +0200

    entradas

--- Actividades de Hilos/Entradas/src/Hilo.java
+++ Actividades de Hilos/Entradas/src/Hilo.java
@@ -1,8 +1,21 @@
+import java.util.Random;
+
 public class Hilo extends Thread {
+    static int entradasDisponibles = 100,entradasVendidas = 0;
+    private int cliente;
+    Random rand = new Random();
+
     @Override
     public void run() {
-
+        int entradas = rand.nextInt(10);
+        if (entradasDisponibles - entradas > 0){
+            entradasVendidas += entradas;
+            entradasDisponibles -= entradas;
+            System.out.println(entradas + " reservadas para Cliente " + this.cliente);
+        }
     }
 
-    public Hilo() {}
+    public Hilo(int cliente) {
+        this.cliente = cliente;
+    }
 }
