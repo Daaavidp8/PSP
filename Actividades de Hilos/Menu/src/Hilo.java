8e4baf3e07ffe5901138c9abce9268b3d44fffc2
davpicroc <davpicroc@alu.edu.gva.es>
2024-10-17 11:46:03 +0200

    menu

--- Actividades de Hilos/Menu/src/Hilo.java
+++ Actividades de Hilos/Menu/src/Hilo.java
@@ -1,22 +1,38 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-public class Primeros extends Thread {
+public class Hilo extends Thread {
+    private int numero;
+
     @Override
     public void run() {
-        FileReader fr = null;
+        String palabra;
         try {
-            fr = new FileReader("menu.txt");
-            int i;
-            while ((i = fr.read()) != -1)
+                File myObj = new File("menu.txt");
+                Scanner myReader = new Scanner(myObj);
+                ArrayList<String> tipos = new ArrayList<>(List.of("PRIMEROS","SEGUNDOS","POSTRES"));
+                ArrayList<String> ficheros = new ArrayList<>(List.of("primeros.txt","segundos.txt","postres.txt"));
+                while (myReader.hasNextLine()) {
+                    String data = myReader.nextLine();
+                    palabra = data.split("-")[1];
+                    if (!tipos.contains(palabra) && this.numero == Integer.parseInt(data.split("-")[0])) {
+                        GuardarPalabra(ficheros.get(this.numero - 1),palabra);
+                        System.out.println("El hilo" + this.numero + " escribe..." + palabra);
+                    }
+                }
+                myReader.close();
 
-                System.out.print((char)i);
-
-            GuardarPalabra(fichero,palabra);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
 
 
+    }
+
+    public Hilo(int numero) {
+        this.numero = numero;
     }
 
     public static void GuardarPalabra(String fichero, String palabra) throws IOException {
--- Actividades de Hilos/Menu/src/Main.java
+++ Actividades de Hilos/Menu/src/Main.java
@@ -1,7 +1,13 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        new Hilo(1).start();
+        new Hilo(2).start();
+        new Hilo(3).start();
     }
 }
